8158de4511c793e988dee6407a5f69c33f4d7429
Zdravko Danailov <zdanailov@kpmg.com>
2019-07-23 10:58:49 +0300

    Finished Build Nested Components

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
-
-@Component({
+import { Component } from '@angular/core';
+@Component ({
   selector: 'pm-root',
   template: `
     <div>
-      <h1>{{ pageTitle }}</h1>
-      <div> My first component</div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
     </div>
   `
 })
+
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+  pageTitle: string = 'Acme product Management';
 }
